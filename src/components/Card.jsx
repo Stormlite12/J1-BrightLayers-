8f5404d9bfb58ef6d254ae5ccf18461b1ae0731b
Siddartha Datta <sid2002official@gmail.com>
2025-01-07 22:32:42 +0530

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,11 +5,11 @@ import PropTypes from "prop-types";
 const Card = forwardRef((props, ref) => {
   return (
     <div ref={ref} className="overflow-x-hidden">
-      <div className="max-w-full w-[12.60431rem] h-[17.33094rem] bg-[#1a3f60] rounded-[0.75388rem] overflow-hidden mx-auto">
-        <div className="w-[10.83656rem] h-[10.93075rem] text-white left-[2.6875rem] top-[2.26156rem] font-pSans text-[0.81669rem] font-light leading-[1.03719rem] tracking-[0.00819rem]">
+      <div className="max-w-full w-[12.60431rem] h-[17.33094rem] bg-[#1a3f60] rounded-[0.75388rem] overflow-hidden mx-auto p-4">
+        <div className="w-[10.83656rem] h-[10.93075rem] text-white font-pSans text-[0.81669rem] font-light leading-[1.03719rem] tracking-[0.00819rem]">
           {props.content}
         </div>
-        <div className="w-[10.83656rem] h-[2.54425rem] left-[2.6875rem] bottom-[1.19363rem]">
+        <div className="w-[10.83656rem] h-[2.54425rem]">
             <div className="text-white font-pSans text-[0.75388rem] font-semibold leading-[0.95738rem] tracking-[0.00756rem]">{props.name}</div>
             <div className="text-white font-pSans text-[0.691rem] font-light tracking-[0.00694rem] leading-[0.95738rem]">{props.title}</div>
         </div>
