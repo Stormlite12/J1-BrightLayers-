da892bf78c850d31d2c41f3e4a96dbfe8fe71476
Siddartha Datta <sid2002official@gmail.com>
2025-01-10 21:21:14 +0530

    updated

--- src/components/PeopleSaying.jsx
+++ src/components/PeopleSaying.jsx
@@ -5,42 +5,34 @@ import image27 from "../assets/image27.png";
 
 function PeopleSaying() {
   return (
-    <div className="mt-[10.84rem] md:mt-[15.69rem] xl:mt-[17.08rem] flex flex-col">
-      <div className="flex justify-center w-[15.81875rem] h-[1.19069rem] mx-auto
-                      md:w-[25.92594rem] md:h-[1.68038rem]">
-        <div className="text-[#313338] text-center font-pSans text-[1.02056rem] md:text-[1.71294rem] font-semibold leading-[2.02131rem] tracking-[0.01713rem]">
-          What Are People Saying
-        </div>
-      </div>
-      <div className="mt-[2.08rem] md:mt-[6.42rem] xl:mt-[4.72rem] mx-auto">
-        <div className="flex flex-row space-x-[1.45rem] md:space-x-[5rem] w-auto ">
-          <img
-            src={image26}
-            alt="eurekaLogo"
-            className="w-[6.01344rem] h-[3.52513rem]
-                      md:w-[7.2685rem]  md:h-[5.85188rem]
-                       lg:w-[12.2685rem] lg:h-[6.85188rem]
-                       xl:w-[13.80206rem] xl:h-[7.70831rem]"
-          />
-          <img
-            src={image25}
-            alt="thamesLogo"
-            className="w-[7.4245rem] h-[2.00113rem] mt-3 md:mt-6
-                      md:w-[10.55188rem] md:h-[2.79444rem]
-                      lg:w-[11.85188rem] lg:h-[3.19444rem]
-                      xl:w-[13.33331rem] xl:h-[3.59375rem]"
-          />
-          <img
-            src={image27}
-            alt="tunvay"
-            className="w-[4.66319rem] h-[3.75838rem] -mt-3 md:-mt-6
-                      md:w-[9.83331rem] md:h-[6.68519rem]
-                       lg:w-[10.83331rem] lg:h-[7.68519rem]
-                       xl:w-[12.1875rem] xl:h-[8.64581rem]"
-          />
-        </div>
+    <div className="mt-[10.84rem] md:mt-[15.69rem] xl:mt-[17.08rem] flex flex-col w-full mx-auto">
+    <div className="flex justify-center w-full max-h-[1.19069rem] mx-auto
+                    md:max-h-[1.68038rem]">
+      <div className="text-[#313338] text-center font-pSans text-[1.02056rem] md:text-[1.71294rem] font-semibold leading-[2.02131rem] tracking-[0.01713rem]">
+        What Are People Saying
       </div>
     </div>
+    <div className="mt-[2.08rem] md:mt-[6.42rem] xl:mt-[4.72rem] w-auto mx-auto">
+      <div className="flex flex-row space-x-[1.45rem] md:space-x-[5rem] justify-center">
+        <img
+          src={image26}
+          alt="eurekaLogo"
+          className="w-[20%] h-auto object-contain"
+        />
+        <img
+          src={image25}
+          alt="thamesLogo"
+          className="w-[25%] h-auto object-contain"
+        />
+        <img
+          src={image27}
+          alt="tunvay"
+          className="w-[20%] h-auto object-contain"
+        />
+      </div>
+    </div>
+  </div>
+  
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,9 @@ import ContactForm from "../pages/ContactForm.jsx";
 import Features from "../components/Features.jsx";
 import PeopleSaying from "../components/PeopleSaying.jsx";
 import InfiniteCarousel from "../components/InfiniteCarousel.jsx";
-import founderImg from '../assets/founderImg.png'
+import FounderBio from '../components/FounderBio.jsx'
 import FeaturesPC from '../components/FeaturesPC.jsx';
-import founderImgMobile from "../assets/founderMobile.png";
+
 import BlogSpot from "../components/BlogSpot.jsx";
 import Footer from '../components/Footer.jsx'
 
@@ -193,9 +193,8 @@ function Home() {
         <FeaturesPC/>
       </div>
 
-      <div>
         <PeopleSaying />
-      </div>
+    
 
       <div>
         <InfiniteCarousel />
@@ -205,80 +204,16 @@ function Home() {
         Meet Our Founder
       </div>
 
-      <div className="flex flex-col lg:flex-row justify-center items-center mt-[3.11rem] lg:mt-[6.56rem] mx-auto  md:max-w-[80rem] "> {/*md:mr-[20rem]*/}
-        
-        <div className="w-full lg:max-w-[18.9707rem] mac:max-w-[21.06413rem] xl:max-w-[22.76475rem] ">
-          <img
-            className="object-fill lg:hidden mx-auto"
-            src={founderImgMobile}
-            alt=""
-          />  
-          <img
-                                                              // md:ml-[1.5rem] xl:ml-[2.5rem] md:mr-[3rem] xl:mr-[6rem]
-            className="object-fill h-auto hidden lg:block"                       
-          src={founderImg} alt="" />
-        </div>
-
-
-        <div className="lg:ml-[1.68rem] lg:mr-[2.67rem]  w-[19.98319rem] lg:w-[10.757rem] mac:w-[11.94406rem] xl:w-[12.90838rem]  lg:flex flex-col lg:h-[21.602rem] mac:h-[23.98581rem] xl:h-[24.77rem] justify-between mt-[3rem] lg:mt-0">
-            <div className="">
-            <p className="mb-[0.48rem] font-pSans font-semibold text-left text-[#565656]
-            w-[19.98319rem] h-[1.39294rem] text-[1.25369rem] leading-[1.24119rem] tracking-[-0.01256rem] 
-            lg:w-[10.757rem] lg:h-[4.16069rem] lg:text-[2.23994rem] lg:leading-[2.2175rem] lg:tracking-[-0.02238rem]
-            mac:w-[11.94406rem] mac:h-[4.8831rem] mac:text-[2.35rem] mac:leading-[2.2rem] mac:tracking-[-0.02225rem]
-             xl:w-[12.90838rem] xl:h-[4.99281rem] xl:text-[2.42075rem] xl:leading-[2.39656rem] xl:tracking-[-0.02419rem]">
-              Pooja Bhadoriya
-            </p>
-            <p className="w-[19.98319rem] h-[1.01794rem] font-title text-[#727272]
-            text-[0.64288rem] font-normal leading-normal text-left
-            lg:w-[9.33944rem] lg:h-[1.63906rem] lg:text-[0.7565rem]
-            mac:w-[10.37006rem] mac:h-[1.81994rem] mac:text-[0.84rem]
-            xl:w-[11.20725rem] xl:h-[1.96688rem] xl:text-[0.90781rem]">
-              Founder & Creative Director
-            </p>
-            </div>
-          
-            <div>
-            <p className="hidden text-[#546660] lg:block font-normal
-            lg:w-[11.13225rem] lg:h-[5.75775rem] lg:text-[1.15125rem]  lg:leading-[1.39938rem] lg:tracking-[0.0115rem]
-            mac:w-[12.36069rem] mac:h-[6.39313rem] mac:text-[1.27825rem]  mac:leading-[1.47825rem] mac:tracking-[0.01281rem]
-            xl:w-[13.35869rem] xl:h-[6.90925rem] xl:text-[1.3815rem]  xl:leading-[1.4919rem] xl:tracking-[0.01381rem]">
-            {`"`}Oh also, she&apos;s really excited to meet you, fellow business-owner.{`"`}
-            </p>
-            </div>
+      <div className="max-w-[90%] mx-auto mt-[3.63rem] lg:mt-0"> 
+      
          
-        </div>
+      <FounderBio/>
+        
+      <BlogSpot/>
 
-
-
-        <div className="w-[19.98319rem] h-[14.68rem] 
-                        lg:w-[30.01956rem] lg:h-[19.3325rem]
-                        mac:w-[33.33225rem] mac:h-[21.46588rem]
-                        xl:w-[34.02338rem] xl:h-[23.199rem]
-                        mt-[1.82rem] md:mt-0                  
-          ">
-            {/* lg:w-[37.50013rem] lg:h-[24.15rem] */}
-            <div className="max-w-[120rem] w-[19.98319rem] h-[10.28625rem] font-title text-[#727171] text-[0.69644rem] font-normal leading-[1.02381rem] tracking-[0.00694rem]
-            lg:w-[30.50013rem] lg:h-[19.3325rem] lg:text-[1.09269rem] lg:leading-[1.497rem] lg:tracking-[0.01094rem] 
-            mac:w-[33.33225rem] mac:h-[21.46588rem] mac:text-[1.21331rem] mac:leading-[1.66219rem ] mac:tracking-[0.01213rem] 
-            xl:w-[40rem] xl:h-[28.97994rem] xl:text-[1.338rem] xl:leading-[1.74406rem] xl:tracking-[0.0238rem]"> 
-            {/* lg:w-[37.50013rem] lg:h-[24.15rem] lg:text-[1.365rem] lg:leading-[1.87006rem] lg:tracking-[0.01363rem] */}
-            
-             Full time overthinker and part time Founder, Pooja launched The Bright
-              Layers in 2019 as a tribute to her experience of working in social
-              media - be it as a designer, writer, creative director, or just as a
-              consultant to her uncle who really wanted his mithai business to go
-              viral on Instagram. <br /> <br /> As a business owner herself, she understands the
-              passion that fuels a brand. Her goal is to bring that passion to the
-              forefront, and to make businesses feel more human.
-            </div>
-            <div className="ml-auto lg:hidden text-[#546660] text-[0.71544rem] w-[11.572rem] h-[2.30369rem] font-normal leading-[0.93181rem] tracking-[0.00713rem] ">
-            {`"`}Oh also, she&apos;s really excited to meet you, fellow business-owner.{`"`}
-            </div>
-        </div>   
       </div>
 
-      <BlogSpot/>
+      
 
       <Footer/>
     </div>
@@ -286,3 +221,5 @@ function Home() {
 }
 
 export default Home;
+  
+
